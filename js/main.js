77b79a8dee3c19063742938d5798bed387ddc70b
“Jonny <jonny@debut.careers>
2017-12-11 14:52:28 +0000

    Index updates

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.feedback-reccomendations').slick({
+    infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  });
+
+  $('.company-logos').slick({
+    infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  });
+});
