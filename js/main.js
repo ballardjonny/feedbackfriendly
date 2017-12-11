0b852f106a47734c42ee9dc9f0525921fd0b689a
“Jonny <jonny@debut.careers>
2017-12-11 15:04:24 +0000

    index html update

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
     infinite: true,
   slidesToShow: 4,
   slidesToScroll: 4,
+  dots: true,
   });
 
   $('.company-logos').slick({
     infinite: true,
   slidesToShow: 6,
   slidesToScroll: 6,
+  dots: true,
   });
 });
