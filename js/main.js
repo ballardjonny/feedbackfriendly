8b071d2785d941a8829a70e2282f9ea85d93563c
“Jonny <jonny@debut.careers>
2017-12-12 13:57:45 +0000

    Main.js

--- js/main.js
+++ js/main.js
@@ -16,8 +16,6 @@ $(document).ready(function(){
       breakpoint: 768,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
         slidesToShow: 3
       }
     },
@@ -25,8 +23,6 @@ $(document).ready(function(){
       breakpoint: 480,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
         slidesToShow: 1
       }
     }
