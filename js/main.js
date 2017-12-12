cfc99e7b91d79af1b7da36ed3f0da9bc81833698
“Jonny <jonny@debut.careers>
2017-12-12 13:55:36 +0000

    Update to Main JS

--- js/main.js
+++ js/main.js
@@ -11,6 +11,26 @@ $(document).ready(function(){
   slidesToShow: 6,
   slidesToScroll: 6,
   dots: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
   });
 
 $('.modal-one').click(function () {
